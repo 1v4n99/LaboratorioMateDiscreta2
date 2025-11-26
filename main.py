40481f79880d8d68840ca004f034372ab82d9acd
said <75824310@continental.edu.pe>
2025-11-26 11:56:03 -0500

    algo gpzu

--- main.py
+++ main.py
@@ -5,16 +5,5 @@ print("tarbajo de trago franklin")
 
 
 print ("vassa chupetin")
-print ("algo gozu")
-<<<<<<< HEAD
-print ("go teka")
-print ("AnthonyGutierrez")
-
-=======
-<<<<<<< HEAD
-print ("primer intento")
-=======
->>>>>>> b8da2d0b414ae3b97409e6a9a6b78cfbbc5a6d32
 print ("go centro")
  da4ddfd76a7f85bec48fe7431a736bd69f915602
->>>>>>> 0e2748f3d532d63f08c41865fd3122cc1701acca
